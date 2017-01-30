b35ab94ebf5bc5696391e38faeef8a9341c4f512
Rogelio Alatorre <rojo.perfeccion@gmail.com>
2017-01-30 08:03:58 -0600

    Add assets, styles and angular-ui-router

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,31 @@
 (function(){
 
-  var app = angular.module('beauty',[]);
+  var app = angular.module('beauty',['ui.router']);
+
+  app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+    .state('productos',{
+      url: '/productos',
+      templateUrl: 'views/productos.html'
+    })
+    .state('registro',{
+      url: '/registro',
+      templateUrl: 'views/registro.html'
+    })
+  });
 
   app.controller('productsController', function($http){
     $http.get('/ObtenerProductos').then((response) => {
       this.products = response.data;
     });
+  });
+
+  $(function(){
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    Materialize.updateTextFields();
+
   });
 
 })();
